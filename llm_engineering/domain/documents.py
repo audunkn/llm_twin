db68623c165cdf4fffa2f54c49601c3f593c15f8
audunkn <audun.klarholm.nilsen@gmail.com>
2025-01-07 13:20:36 +0100

    added class Document and class RepositoryDocument to documents.py

--- llm_engineering/domain/documents.py
+++ llm_engineering/domain/documents.py
@@ -17,3 +17,16 @@ class UserDocument(NoSQLBaseDocument):
     @property
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
+    
+class Document(NoSQLBaseDocument, ABC):
+    content: dict
+    platform: str
+    author_id: UUID4 = Field(alias="author_id")
+    author_full_name: str = Field(alias="author_full_name")
+
+class RepositoryDocument(Document):
+    name: str
+    link: str
+
+    class Settings:
+        name = DataCategory.REPOSITORIES
