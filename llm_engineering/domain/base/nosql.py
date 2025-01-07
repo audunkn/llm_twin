e147af188a512bda0d483ee62f0b7041a5a790fe
audunkn <audun.klarholm.nilsen@gmail.com>
2025-01-08 00:16:14 +0100

    added imports to nosql.py, added database connection

---
+++ llm_engineering/domain/base/nosql.py
@@ -0,0 +1,13 @@
+import uuid
+from abc import ABC
+from typing import Generic, Type, TypeVar
+
+from loguru import logger
+from pydantic import UUID4, BaseModel, Field
+from pymongo import errors
+
+from llm_engineering.domain.exceptions import ImproperlyConfigured
+from llm_engineering.infrastructure.db.mongo import connection
+from llm_engineering.settings import settings
+
+_database = connection.get_database(settings.DATABASE_NAME)
