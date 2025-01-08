f9a36c7d55338f06448bf6e385ab74f1f3c27327
audunkn <audun.klarholm.nilsen@gmail.com>
2025-01-08 01:05:35 +0100

    added classmethod def from_mongo to nosql.py

--- llm_engineering/domain/base/nosql.py
+++ llm_engineering/domain/base/nosql.py
@@ -11,3 +11,34 @@ from llm_engineering.infrastructure.db.mongo import connection
 from llm_engineering.settings import settings
 
 _database = connection.get_database(settings.DATABASE_NAME)
+
+T = TypeVar("T", bound="NoSQLBaseDocument") # signature, for inheritance tracking purposes
+
+class NoSQLBaseDocument(BaseModel, Generic[T], ABC):
+    id: UUID4 = Field(default_factory=uuid.uuid4)
+
+    # Compares to instances of classes to check for equality of UUID4 ids
+    # Usage: Duplicate control
+    def __eq__(self, value: object) -> bool:
+        if not isinstance(value, self.__class__):
+            return False
+
+        return self.id == value.id
+    
+    def __hash__(self) -> int:
+        return hash(self.id)
+    
+    @classmethod
+    def from_mongo(cls: Type[T], data: dict) -> T:
+        """Convert "_id" (str object) into "id" (UUID object)."""
+
+        if not data:
+            raise ValueError("Data is empty.")
+
+        id = data.pop("_id")
+
+        return cls(**dict(data, id=id))
+
+
+
+
