1e5ed79ee2ba87f9c7141f0a4d9d2920bb01c843
audunkn <audun.klarholm.nilsen@gmail.com>
2025-01-09 20:46:56 +0100

    small fix

--- steps/etl/crawl_links.py
+++ steps/etl/crawl_links.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 from typing_extensions import Annotated
 from zenml import get_step_context, step
 
-from llm_engineering.application.crawlers.dispatcher import CrawlerDispatcher # TODO - implement
+from llm_engineering.application.crawlers.dispatcher import CrawlerDispatcher
 from llm_engineering.domain.documents import UserDocument
 
 @step
