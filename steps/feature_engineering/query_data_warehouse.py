3af645db2ed1aa03d2154718b408e692c906cb83
audunkn <audun.klarholm.nilsen@gmail.com>
2025-01-10 11:09:28 +0100

    added func query_data_warehouse to query_data_warehouse.py

---
+++ steps/feature_engineering/query_data_warehouse.py
@@ -0,0 +1,34 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+from loguru import logger
+from typing_extensions import Annotated
+from zenml import get_step_context, step
+
+from llm_engineering.application import utils
+from llm_engineering.domain.base.nosql import NoSQLBaseDocument
+from llm_engineering.domain.documents import ArticleDocument, Document, PostDocument, RepositoryDocument, UserDocument
+
+
+@step
+def query_data_warehouse(
+    author_full_names: list[str],
+) -> Annotated[list, "raw_documents"]:
+    documents = []
+    authors = []
+    for author_full_name in author_full_names:
+        logger.info(f"Querying data warehouse for user: {author_full_name}")
+
+        first_name, last_name = utils.split_user_full_name(author_full_name)
+        logger.info(f"First name: {first_name}, Last name: {last_name}")
+        user = UserDocument.get_or_create(first_name=first_name, last_name=last_name)
+        authors.append(user)
+
+        results = fetch_all_data(user)
+        user_documents = [doc for query_result in results.values() for doc in query_result]
+
+        documents.extend(user_documents)
+
+    step_context = get_step_context()
+    step_context.add_output_metadata(output_name="raw_documents", metadata=_get_metadata(documents))
+
+    return documents
